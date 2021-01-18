b24aa8fa1ab46f3bc7c69eeb4730a29a5cc48668
sunnyegg <adilatresna21@gmail.com>
2021-01-18 15:25:07 +0700

    discount ok

--- src/app/pages/Report/DiscountTab.js
+++ src/app/pages/Report/DiscountTab.js
@@ -8,7 +8,6 @@ import "../style.css";
 
 export const DiscountSalesTab = ({ selectedOutlet, startDate, endDate }) => {
   const [allPromoSales, setAllPromoSales] = React.useState([]);
-  // const [allCategories, setAllCategories] = React.useState([]);
 
   const getDiscountSales = async (id, start_range, end_range) => {
     const API_URL = process.env.REACT_APP_API_URL;
@@ -43,27 +42,9 @@ export const DiscountSalesTab = ({ selectedOutlet, startDate, endDate }) => {
     }
   };
 
-  // const getCategories = async () => {
-  //   const API_URL = process.env.REACT_APP_API_URL;
-  //   try {
-  //     const { data } = await axios.get(`${API_URL}/api/v1/product-category`);
-  //     const categories = data.data.map((item) => item.name);
-  //     setAllCategories(categories);
-  //   } catch (err) {
-  //     if (err.response.status === 404) {
-  //       setAllCategories([]);
-  //     }
-  //     console.log(err);
-  //   }
-  // };
-
   React.useEffect(() => {
     getDiscountSales(selectedOutlet.id, startDate, endDate);
   }, [selectedOutlet, startDate, endDate]);
-
-  // React.useEffect(() => {
-  //   getCategories();
-  // }, []);
 
   const promoSalesData = () => {
     const data = [];
@@ -71,79 +52,149 @@ export const DiscountSalesTab = ({ selectedOutlet, startDate, endDate }) => {
     const completedTransactions = allPromoSales.filter(
       (item) => item.Payment?.status === "done"
     );
-
     const paymentPromos = completedTransactions.filter(
       (item) => item.Payment?.Payment_Promos.length
     );
-    // const countSpecialSold = specialPromoSold.map((item, index) => {
-    //   return item.reduce((init, curr) => {
-    //     const filterPromo = completedTransactions[
-    //       index
-    //     ].Payment.Payment_Promos.filter((val) => val.Promo.name === curr);
-    //     init[curr] = filterPromo.reduce(
-    //       (initItem, currItem) => (initItem += currItem || 0),
-    //       0
-    //     );
-    //     return init;
-    //   }, {});
-    // });
 
-    console.log(paymentPromos);
-    // console.log(countSpecialSold);
-    // const countTypesTotal = promoSold.map((item, index) => {
-    //   return item.reduce((init, curr) => {
-    //     const filterProduct = completedTransactions[
-    //       index
-    //     ].Transaction_Items.filter(
-    //       (prod) => prod.Product.Product_Category.name === curr
-    //     );
-    //     init[curr] = filterProduct.reduce(
-    //       (initItem, currItem) =>
-    //         (initItem +=
-    //           currItem.quantity *
-    //           (currItem.price_product -
-    //             currItem.price_discount +
-    //             currItem.price_service)),
-    //       0
-    //     );
-    //     return init;
-    //   }, {});
-    // });
+    const automaticPromos = paymentPromos
+      .map((item) =>
+        item.Payment?.Payment_Promos.filter((val) => val.Promo.Automatic_Promo)
+      )
+      .flat(1);
+    const specialPromos = paymentPromos
+      .map((item) =>
+        item.Payment?.Payment_Promos.filter((val) => val.Promo.Special_Promo)
+      )
+      .flat(1);
+    const voucherPromos = paymentPromos
+      .map((item) =>
+        item.Payment?.Payment_Promos.filter((val) => val.Promo.Voucher_Promo)
+      )
+      .flat(1);
 
-    // const categorySold = allCategories.reduce((init, curr) => {
-    //   init[curr] = countTypesSold.reduce(
-    //     (initItem, currItem) => (initItem += currItem[curr] || 0),
-    //     0
-    //   );
-    //   return init;
-    // }, {});
-    // const categoryTotal = allCategories.reduce((init, curr) => {
-    //   init[curr] = countTypesTotal.reduce(
-    //     (initItem, currItem) => (initItem += currItem[curr] || 0),
-    //     0
-    //   );
-    //   return init;
-    // }, {});
+    const countAutomaticPromo = automaticPromos.reduce((init, curr) => {
+      init[curr.Promo.Automatic_Promo.name] =
+        (init[curr.Promo.Automatic_Promo.name] || 0) + 1;
 
-    // allCategories.forEach((category) => {
-    //   data.push({
-    //     category,
-    //     usage: categorySold[category],
-    //     refunded: categoryRefund[category],
-    //     total: categoryTotal[category]
-    //   });
-    // });
+      let valuePromo = init["value_promo"] || 0;
 
-    // data.sort((a, b) => b.usage - a.usage);
+      if (curr.type === "percentage") {
+        const currTransaction = paymentPromos.find(
+          (item) => item.Payment.id === curr.payment_id
+        );
+        const totalTrans = currTransaction.Transaction_Items.reduce(
+          (initTrans, currTrans) => (initTrans += currTrans.price_total),
+          0
+        );
+        valuePromo += (totalTrans * curr.value) / 100;
+      } else {
+        valuePromo += curr.value;
+      }
 
-    // const totalUsage = data.reduce((init, curr) => (init += curr.usage), 0);
-    // const totalAmount = data.reduce((init, curr) => (init += curr.total), 0);
+      init["value_promo"] = valuePromo;
+      return init;
+    }, {});
+    const countSpecialPromo = specialPromos.reduce((init, curr) => {
+      init[curr.Promo.Special_Promo.name] =
+        (init[curr.Promo.Special_Promo.name] || 0) + 1;
 
-    // data.push({
-    //   name: "",
-    //   usage: totalUsage,
-    //   total: totalAmount
-    // });
+      let valuePromo = init["value_promo"] || 0;
+
+      if (curr.type === "percentage") {
+        const currTransaction = paymentPromos.find(
+          (item) => item.Payment.id === curr.payment_id
+        );
+        const totalTrans = currTransaction.Transaction_Items.reduce(
+          (initTrans, currTrans) => (initTrans += currTrans.price_total),
+          0
+        );
+        valuePromo += (totalTrans * curr.value) / 100;
+      } else {
+        valuePromo += curr.value;
+      }
+
+      init["value_promo"] = valuePromo;
+      return init;
+    }, {});
+    const countVoucherPromo = voucherPromos.reduce((init, curr) => {
+      init[curr.Promo.Voucher_Promo.name] =
+        (init[curr.Promo.Voucher_Promo.name] || 0) + 1;
+
+      let valuePromo = init["value_promo"] || 0;
+
+      if (curr.type === "percentage") {
+        const currTransaction = paymentPromos.find(
+          (item) => item.Payment.id === curr.payment_id
+        );
+        const totalTrans = currTransaction.Transaction_Items.reduce(
+          (initTrans, currTrans) => (initTrans += currTrans.price_total),
+          0
+        );
+        valuePromo += (totalTrans * curr.value) / 100;
+      } else {
+        valuePromo += curr.value;
+      }
+
+      init["value_promo"] = valuePromo;
+      init["quota_voucher"] = curr.Promo.Voucher_Promo.quota;
+      return init;
+    }, {});
+
+    if (Object.keys(countSpecialPromo).length) {
+      const specialPromo = {};
+      for (const key of Object.keys(countSpecialPromo)) {
+        if (key === "value_promo") {
+          specialPromo.total = countSpecialPromo[key];
+        } else {
+          specialPromo.name = key;
+          specialPromo.usage = countSpecialPromo[key];
+        }
+      }
+      data.push(specialPromo);
+    }
+
+    if (Object.keys(countAutomaticPromo).length) {
+      const automaticPromo = {};
+      for (const key of Object.keys(countAutomaticPromo)) {
+        if (key === "value_promo") {
+          automaticPromo.total = countAutomaticPromo[key];
+        } else {
+          automaticPromo.name = key;
+          automaticPromo.usage = countAutomaticPromo[key];
+        }
+      }
+      data.push(automaticPromo);
+    }
+
+    if (Object.keys(countVoucherPromo).length) {
+      const automaticPromo = {};
+      for (const key of Object.keys(countVoucherPromo)) {
+        if (key === "value_promo") {
+          automaticPromo.total = countVoucherPromo[key];
+        } else if (key === "quota_voucher") {
+          automaticPromo.quota = countVoucherPromo[key];
+        } else {
+          automaticPromo.name = key;
+          automaticPromo.usage = countVoucherPromo[key];
+        }
+      }
+      data.push(automaticPromo);
+    }
+
+    const totalUsage = data.reduce(
+      (init, curr) => (init += curr.usage || 0),
+      0
+    );
+    const totalAmount = data.reduce(
+      (init, curr) => (init += curr.total || 0),
+      0
+    );
+
+    data.push({
+      name: "",
+      usage: totalUsage,
+      total: totalAmount
+    });
 
     return data;
   };
@@ -155,7 +206,7 @@ export const DiscountSalesTab = ({ selectedOutlet, startDate, endDate }) => {
           <tr>
             <th></th>
             <th>Discount Name</th>
-            <th>Usage</th>
+            <th>Total Usage</th>
             <th>Total Collected</th>
           </tr>
         </thead>
@@ -165,7 +216,9 @@ export const DiscountSalesTab = ({ selectedOutlet, startDate, endDate }) => {
               <tr key={index}>
                 <td></td>
                 <td>{item.name}</td>
-                <td>{item.usage}</td>
+                <td>
+                  {item.quota ? `${item.usage}/${item.quota}` : item.usage}
+                </td>
                 <td>{rupiahFormat.convert(item.total)}</td>
               </tr>
             );
